caf2b8ef683a957c2bf9538afed6712415014f9e
Afshin <100291026+ONZE-11@users.noreply.github.com>
2022-12-17 18:36:45 -0500

    Add files via upload
    this is a dice game

---
+++ taas.py
@@ -0,0 +1,22 @@
+
+import random
+
+list=[1,2,3,4,5,6]
+random.shuffle(list)
+print(list)
+
+exit=False
+while (not exit):
+ 
+    x=int(input('enter youer gusse number between 1 to 6 : '))
+    r=random.choice(list)
+    def taas ():
+        if x==r:
+            return(f'you are win ! **{r}** is correct')
+        elif x>6 or x<1 :
+            exit=True
+        else:
+            return (f'you are lose >>{r}<< is real number !')
+    
+    print(taas())
+
